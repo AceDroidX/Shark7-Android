496b00f0537bdb56491c0debd8ffb6b58b855fef
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2023-04-26 00:07:17 +0800

    修复通知折叠

--- app/src/main/java/io/github/acedroidx/shark7/MyNotificationManager.kt
+++ app/src/main/java/io/github/acedroidx/shark7/MyNotificationManager.kt
@@ -1,11 +1,15 @@
 package io.github.acedroidx.shark7
 
+import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -14,12 +18,13 @@ import javax.inject.Singleton
 
 @Singleton
 class MyNotificationManager @Inject constructor(@ApplicationContext val context: Context) {
+    var notificationId = 100
     fun createAlarmChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create the NotificationChannel
             val name = "AlarmService"
 //            val descriptionText = ""
-            val importance = NotificationManager.IMPORTANCE_MAX
+            val importance = NotificationManager.IMPORTANCE_HIGH
             val mChannel = NotificationChannel("AlarmService", name, importance)
             mChannel.setSound(null, null)
             mChannel.enableVibration(false)
@@ -50,13 +55,22 @@ class MyNotificationManager @Inject constructor(@ApplicationContext val context:
 
     fun sendEvent(event: Shark7Event) {
         createEventChannel()
-        val notification: Notification = NotificationCompat.Builder(context, "EventChannel")
-            .setContentTitle(event.getTitle())
-            .setContentText(event.msg)
-            .setStyle(NotificationCompat.BigTextStyle()
-                .bigText(event.msg))
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
-        NotificationManagerCompat.from(context).notify(10, notification)
+        val notification: Notification =
+            NotificationCompat.Builder(context, "EventChannel")
+                .setContentTitle(event.getTitle())
+                .setContentText(event.msg)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(event.msg))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setGroup("io.github.acedroidx.shark7.EVENT_GROUP")
+                .build()
+        if (ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.w("sendEvent", "Manifest.permission.POST_NOTIFICATIONS not granted")
+            return
+        }
+        NotificationManagerCompat.from(context).notify(notificationId, notification)
+        notificationId++
     }
 }
